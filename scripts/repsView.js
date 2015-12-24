1d0f97052e6780527f664963fc59f12d52a12188
David Aarons <daarons@users.noreply.github.com>
2015-12-24 17:32:14 -0500

    Update repsView.js

--- scripts/repsView.js
+++ scripts/repsView.js
@@ -168,11 +168,8 @@ function writeRepNode(name, officeName, party, phone, site, photo, address) {
 function renderResults(response, rawResponse) {
     // apparently this function only runs if 
     // it's online, not local.
-    var david = "david aarons";
-    console.log(response);
     var locationId = document.getElementById('locationBlock');
     if (!response || response.error) {
-        console.log("error with response");
         locationId.innerHTML = '<div class = "alert alert-danger">Sorry, we were unable to locate information for the address entered. <a href = "index.html" class = "alert-link"><br>Try again?</a></div>';
         return;
     }
