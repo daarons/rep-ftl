cf403cc6c5594efaa89d8f8fffb26d90508e7c68
David Aarons <daarons@users.noreply.github.com>
2015-12-24 17:02:42 -0500

    Update repsView.js

--- scripts/repsView.js
+++ scripts/repsView.js
@@ -169,7 +169,7 @@ function renderResults(response, rawResponse) {
     // apparently this function only runs if 
     // it's online, not local.
     var david = "david aarons";
-    console.log(response.status);
+    console.log(JSON.parse(response));
     var locationId = document.getElementById('locationBlock');
     if (response.status = 404) {
         console.log("error with response");
