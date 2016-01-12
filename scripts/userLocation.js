3b0e84d4a6ff8663d842dc10e1aa1faff6c35b0b
David Aarons <daarons@users.noreply.github.com>
2016-01-12 15:24:34 -0500

    Update userLocation.js

--- scripts/userLocation.js
+++ scripts/userLocation.js
@@ -40,6 +40,7 @@ function geolocateUser() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            console.log("David: " + getBounds());
             writeAddressName(pos); //comment this line to stop the page from reload when debugging 
         });
     } 
